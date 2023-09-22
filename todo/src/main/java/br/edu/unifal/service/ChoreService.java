3fdde1d76de170261f0accc43465a609362679e0
nicolelimat <nicolelima.eu@gmail.com>
2023-09-22 15:13:51 -0300

    test: Add unit tests for ChoreService

---
+++ todo/src/main/java/br/edu/unifal/service/ChoreService.java
@@ -0,0 +1,12 @@
+package br.edu.unifal.service;
+
+import br.edu.unifal.domain.Chore;
+
+import java.time.LocalDate;
+
+public class ChoreService {
+
+    public Chore addChore(String description, LocalDate deadline){
+        return null;
+    }
+}
